bf059554b8ba5bd7b32e1b7337ce7e7a1526b243
Alin Screciu <alin.screciu01@gmail.com>
2024-01-08 19:27:31 +0200

    Work on login + register

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -5,7 +5,7 @@ import {
   type DefaultSession,
   type NextAuthOptions,
 } from "next-auth";
-import DiscordProvider from "next-auth/providers/discord";
+import Google from "next-auth/providers/google";
 
 import { env } from "~/env";
 import { db } from "~/server/db";
@@ -37,6 +37,9 @@ declare module "next-auth" {
  * @see https://next-auth.js.org/configuration/options
  */
 export const authOptions: NextAuthOptions = {
+  pages: {
+    newUser: "/register"
+  },
   callbacks: {
     session: ({ session, user }) => ({
       ...session,
@@ -45,12 +48,13 @@ export const authOptions: NextAuthOptions = {
         id: user.id,
       },
     }),
+
   },
   adapter: PrismaAdapter(db),
   providers: [
-    DiscordProvider({
-      clientId: env.DISCORD_CLIENT_ID,
-      clientSecret: env.DISCORD_CLIENT_SECRET,
+    Google({
+      clientId: env.GOOGLE_CLIENT_ID,
+      clientSecret: env.GOOGLE_CLIENT_SECRET,
     }),
     /**
      * ...add more providers here.
